a4f9e91cf141a16e8bfe9e8a25fbb36d2b675627
Stacy Nowak <nowak.stacy@gmail.com>
2019-02-28 20:45:43 -0800

    added functions to javascript for resetting the game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,19 +1,24 @@
 // Array of Possible Words to guess
 wordBank = ["GIRAFFE", "ZEBRA", "LION", "TIGER", "ORANGUTAN", "TAPIR", "GORILLA", "JAGUAR", "TOUCAN", "BEAR", "WOLF", "PORCUPINE", "WALLABY", "KANGAROO", "RHINO"];
+const letters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 
 // Declare variables
 let computerAnswer ;
 let answerLetters = [];
 let answerLength ;
 let userGuess ;
-const letters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 let pastGuesses = [] ;
 let matchCount = 0;
-let wins = 0;
-let losses = 0 ;
-let guessesLeft = 10;
+let wins = 0; 
+let losses = 0 ; 
+let guessesLeft = 10;  
 
 
+function gameStart () {
+// Reset Variables
+pastGuesses = [] ;
+matchCount = 0;
+guessesLeft = 10;
 
 // Computer decides a random word for the game
 computerAnswer = wordBank[Math.floor(Math.random() * wordBank.length)];
@@ -22,9 +27,12 @@ console.log("Answer: " + computerAnswer);
 // Split computer answer into individual letters
 answerLetters = computerAnswer.split("");
 console.log("Answer Letters: " + answerLetters);
+
 answerLength = answerLetters.length;
 console.log("Answer Length: " + answerLength);
+}
 
+function gamePlay() {
 // Obtain guess from user on keypress, changes to uppercase, & stores in a list of guesses
 document.onkeyup = function (event) {
     userGuess = event.key.toUpperCase();
@@ -34,19 +42,16 @@ document.onkeyup = function (event) {
         alert("You have already guessed that letter, try again.");
     }
 // Checks that answer is a letter
-    else if (!letters.includes(userGuess)) {
-        alert("That is not a letter. Try pressing a letter key.")
-    }
+        else if (!letters.includes(userGuess)) {
+            alert("That is not a letter. Try pressing a letter key.")
+        }
 // If both a new letter and not in the answer, counts down a guess
     else if (!answerLetters.includes(userGuess) && !pastGuesses.includes(userGuess) && letters.includes(userGuess)) {
         pastGuesses.push(userGuess);
         --guessesLeft;
         console.log(pastGuesses);
         console.log("Guesses Left: " + guessesLeft);
-        if (guessesLeft === 0) {
-            losses--;
-        }
-        console.log("Losses: " + losses);
+    gameEnd();
     }
 // A good answer will count toward number of matches in answer word
 // When matches reach the answer length, user wins
@@ -56,10 +61,7 @@ document.onkeyup = function (event) {
             if (answerLetters[x] === userGuess) {
                 matchCount++;
                 console.log("matchCount: " + matchCount);
-                if (matchCount === answerLength) {
-                    wins++;
-                    console.log("Wins: " + wins);
-                }
+        gameEnd();
             }
         }
     }
@@ -67,4 +69,19 @@ document.onkeyup = function (event) {
     document.getElementById("loss").textContent = losses;
     document.getElementById("miss").textContent = guessesLeft;
 }
+}
+function gameEnd() {
+    if (matchCount === answerLength) {
+        wins++;
+        console.log("Wins: " + wins);
+        gameStart();
+    }
+    if (guessesLeft === 0) {
+        losses--;
+        console.log("Losses: " + losses);
+        gameStart();
+    } 
+}
 
+gameStart();
+gamePlay();
