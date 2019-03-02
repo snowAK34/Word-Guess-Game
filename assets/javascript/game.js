159da2a3a1ed55f3c1a28ca8fc6455309d91f1a9
Stacy Nowak <nowak.stacy@gmail.com>
2019-03-01 19:25:16 -0800

    added design elements

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,12 @@
+"use strict"
+
 // Array of Possible Words to guess
-wordBank = ["GIRAFFE", "ZEBRA", "LION", "TIGER", "ORANGUTAN", "TAPIR", "GORILLA", "JAGUAR", "TOUCAN", "BEAR", "WOLF", "PORCUPINE", "WALLABY", "KANGAROO", "RHINO"];
+const wordBank = ["GIRAFFE", "ZEBRA", "LION", "TIGER", "ORANGUTAN", "TAPIR", "GORILLA", "JAGUAR", "TOUCAN", "BEAR", "WOLF", "PORCUPINE", "WALLABY", "KANGAROO", "RHINO"];
 const letters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 
 // Declare variables
 let computerAnswer ;
 let answerLetters = [];
-let answerLength ;
 let userGuess ;
 let pastGuesses = [] ;
 let matchCount = 0;
@@ -18,7 +19,7 @@ let newSlotArr = [];
 let gameRunning = false;
 
 function getGoing() {
-    if (gameRunning == false) {
+    if (gameRunning === false) {
         document.onkeyup = function() {
             gameStart();
         }
@@ -26,69 +27,71 @@ function getGoing() {
 };
 
 function gameStart() {
-// Reset Variables
-pastGuesses = [] ;
-matchCount = 0;
-guessesLeft = 10;
+    gameRunning === true
+    console.log("Running(start of gameStart): " + gameRunning);
 
-// Hide Directions
-document.getElementById("instructions").style.display = "none";
+    // Reset Variables
+    pastGuesses = [] ;
+    guessesLeft = 10;
 
-// Computer decides a random word for the game
-computerAnswer = wordBank[Math.floor(Math.random() * wordBank.length)];
-console.log("Answer: " + computerAnswer);
+    // Hide Directions
+    document.getElementById("instructions").style.display = "none";
 
-// Split computer answer into individual letters
-answerLetters = computerAnswer.split("");
-console.log("Answer Letters: " + answerLetters);
+    // Computer decides a random word for the game
+    computerAnswer = wordBank[Math.floor(Math.random() * wordBank.length)];
+    console.log("Answer: " + computerAnswer);
 
-answerLength = answerLetters.length;
-console.log("Answer Length: " + answerLength);
+    // Split computer answer into individual letters
+    answerLetters = computerAnswer.split("");
+    console.log("Answer Letters: " + answerLetters);
 
-let slotStr = slot.repeat(answerLength);
-slotArr = slotStr.split(" ");
-document.getElementById("letter-slots").textContent = slotArr.join(" ");
+    // Display blanks equal to answer length
+    let slotStr = slot.repeat(answerLetters.length);
+    slotArr = slotStr.split(" ");
+    document.getElementById("letter-slots").textContent = slotArr.join(" ");
 
-gameRunning = true;
-
-gamePlay();
+    gamePlay();
 }
 
 
 function gamePlay() {
-// Obtain guess from user on keypress, changes to uppercase, & stores in a list of guesses
+    gameRunning === true
+    console.log("Running(start of gamePlay): " + gameRunning);
+    
+    // Obtain guess from user on keypress, changes to uppercase, & stores in a list of guesses
     document.onkeyup = function (event) {
         userGuess = event.key.toUpperCase();
 
-    // Check if answer has been tried
+        // Check if answer has been tried
         if (pastGuesses.includes(userGuess)) {
             alert("You have already guessed that letter, try again.");
         }
-    // Checks that answer is a letter
-            else if (!letters.includes(userGuess)) {
-                alert("To make a guess, try pressing a letter key.")
-            }
-    // If both a new letter and not in the answer, counts down a guess
-        else if (!answerLetters.includes(userGuess) && !pastGuesses.includes(userGuess) && letters.includes(userGuess)) {
-            pastGuesses.push(userGuess);
-            --guessesLeft;
-            console.log(pastGuesses);
-            console.log("Guesses Left: " + guessesLeft);
-        gameEnd();
+        // Checks that answer is a letter
+        else if (!letters.includes(userGuess)) {
+            alert("To make a guess, try pressing a letter key.")
         }
-    // A good answer will count toward number of matches in answer word
-    // Display correct letter
-    // When matches reach the answer length, user wins
-        else {
-            pastGuesses.push(userGuess);
-            for (x = 0 ; x < answerLetters.length; x++) {
+        // A good answer will count toward number of matches in answer word
+        // Display correct letter
+        // When matches reach the answer length, user wins
+        else if (answerLetters.includes(userGuess)) {
+            for (let x = 0 ; x < answerLetters.length; x++) {
                 if (answerLetters[x] === userGuess) {
                     slotArr.splice(x, 1, answerLetters[x]);
                     newSlotArr = slotArr.join(" ");
+                    matchCount++;
+                    console.log("matchCount: " + matchCount);
                 }
             }
             document.getElementById("letter-slots").textContent = newSlotArr;
             gameEnd();    
+        }
+        // If both a new letter and not in the answer, counts down a guess
+        else {
+            pastGuesses.push(userGuess);
+            --guessesLeft;
+            console.log("Past Guesses: " + pastGuesses);
+            console.log("Guesses Left: " + guessesLeft);
+        gameEnd();
         }
         document.getElementById("win").textContent = wins;
         document.getElementById("loss").textContent = losses;
@@ -98,25 +101,24 @@ function gamePlay() {
 }
 
 function gameEnd() {
-    if (!newSlotArr.includes("_ ")) {
+    if (matchCount === answerLetters.length) {
         wins++;
         console.log("Wins: " + wins);
-        gameRunning = false;
-        document.onkeyup = function() {
-            gameStart();
-        }
+        gameRunning === false;
+        console.log("Running(win condition): " + gameRunning);
+        getGoing();
     }
-    if (guessesLeft === 0) {
+    else if (guessesLeft === 0) {
         losses--;
         console.log("Losses: " + losses);
-        gameRunning = false;
-        document.onkeyup = function() {
-            gameStart();
-        }
-    } 
-document.getElementById("instructions").style.display = "none";
+        gameRunning == false;
+        console.log("Running(lose condition): " + gameRunning);
+        getGoing();
+    }
+    else {
+        gamePlay();
+    }
 }
 
+console.log("Running(start of code execution): " + gameRunning);
 getGoing();
-
-
